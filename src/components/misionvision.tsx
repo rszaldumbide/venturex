a744222632758dc8c6c1a9ea290a23c660261b7f
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-06-11 20:47:41 -0500

    chore: Actualizar dependencias de npm y agregar biblioteca Embla Carousel React

--- src/components/misionvision.tsx
+++ src/components/misionvision.tsx
@@ -1,25 +1,22 @@
 import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
-import Image from "next/image";
+import { FaBullseye, FaEye } from "react-icons/fa";
 
 interface MissionVisionItem {
-  imgSrc: string;
-  altText: string;
+  icon: JSX.Element;
   title: string;
   description: string;
 }
 
 const missionVisionData: MissionVisionItem[] = [
   {
-    imgSrc: "/imgs/misionvision/mision.jpg",
-    altText: "Misión",
+    icon: <FaBullseye className="text-4xl text-blue-500" />,
     title: "Misión",
     description:
       "Ser una plataforma digital que promueve la expansión internacional dentro del mercado comercial, ofreciendo servicios integrados de inteligencia de negocios, investigación de mercado y asesoramiento legal.",
   },
   {
-    imgSrc: "/imgs/misionvision/vision.jpg",
-    altText: "Visión",
+    icon: <FaEye className="text-4xl text-green-500" />,
     title: "Visión",
     description:
       "En cinco años, Venturex se consolidará como una plataforma líder en la facilitación de conexiones comerciales globales. Aspiramos a ser el puente que derribe las barreras geográficas y permita la internacionalización múltiples empresas.",
@@ -36,14 +33,10 @@ const MissionVisionComponent: React.FC = () => {
             className="rounded-lg border border-gray-200 bg-cyan-50 shadow hover:bg-cyan-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
           >
             <CardContent className="flex flex-col items-center sm:flex-row p-3">
-              <Image
-                src={item.imgSrc}
-                alt={item.altText}
-                width={200}
-                height={300}
-                className="w-full h-auto rounded-t-lg sm:w-1/2 sm:h-auto sm:rounded-none sm:rounded-l-lg"
-              />
-              <div className="flex flex-col justify-between p-4 leading-normal sm:w-1/2">
+              <div className="w-full flex items-center justify-center sm:w-1/4">
+                {item.icon}
+              </div>
+              <div className="flex flex-col justify-between p-4 leading-normal sm:w-3/4">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                   {item.title}
                 </h5>
